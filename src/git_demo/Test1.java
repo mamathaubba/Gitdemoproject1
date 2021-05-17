7778c0670b593db6102d92a6f42069c8e593ea80
mamatha <mamathaub@gmail.com>
2021-05-17 21:32:11 +0530

    Commiting test2 to BranchX

---
+++ src/git_demo/Test1.java
@@ -0,0 +1,20 @@
+package git_demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+        String hungry  =  "yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something...");
+		}
+		else
+		{
+			System.out.println(" Do work...");
+		}
+
+	}
+
+}
