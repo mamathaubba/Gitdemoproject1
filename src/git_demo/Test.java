747e91aaf807f98aea15f9d8d0b724a74e404fe9
mamatha <mamathaub@gmail.com>
2021-05-17 19:58:26 +0530

    This is my first commit

---
+++ src/git_demo/Test.java
@@ -0,0 +1,22 @@
+package git_demo;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		
+		String hungry  =  "yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something...");
+		}
+		else
+		{
+			System.out.println(" Do work...");
+		}
+		
+	}
+	
+	
+}
